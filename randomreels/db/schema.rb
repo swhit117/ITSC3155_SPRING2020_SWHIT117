8000d41c0bf59227e5f43b63b189dd31ff30d57f
Andrew Draper <adraper3@uncc.edu>
2020-04-29 17:31:58 +0000

    fixed migration error

--- randomreels/db/schema.rb
+++ randomreels/db/schema.rb
@@ -12,6 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_04_29_165848) do
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "runTime"
+    t.string "poster"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "runTime"
