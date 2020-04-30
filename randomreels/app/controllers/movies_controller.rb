039cdefbadb8a9aa4ba906050ae39847fac7f265
swhit117 <swhit117@uncc.edu>
2020-04-30 03:24:26 +0000

    Started implementation of movie objects with controllers

--- randomreels/app/controllers/movies_controller.rb
+++ randomreels/app/controllers/movies_controller.rb
@@ -1,2 +1,7 @@
 class MoviesController < ApplicationController
+    def new
+        @movie = ApiMovieTrack.new();
+        # Movie.write_attribute(:title, "Avengers Endgame")
+        redirect_to new_page_path
+    end
 end
